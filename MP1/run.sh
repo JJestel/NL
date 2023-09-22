b49dea50714006cdda032d4ca0ac3d0a42148bf4
Jascha Jestel <jj.jestel@gmail.com>
2023-09-22 22:56:17 +0100

    added propper tests

--- MP1/run.sh
+++ MP1/run.sh
@@ -14,7 +14,7 @@ done
 
 fstconcat compiled/mmm2mm.fst compiled/mix2numerical_dy.fst > compiled/mix2numerical.fst
 
-fstinvert compiled/pt2en.fst > compiled/en2pf.fst
+fstinvert compiled/pt2en.fst > compiled/en2pt.fst
 
 fstconcat compiled/month.fst compiled/slash.fst > compiled/temp0.fst
 fstconcat compiled/temp0.fst compiled/day.fst > compiled/temp1.fst
@@ -40,80 +40,52 @@ done
 # ############      3 different ways of testing     ############
 # ############ (you can use the one(s) you prefer)  ############
 
-# #1 - generates files
-# echo "\n***********************************************************"
-# echo "Testing 4 (the output is a transducer: fst and pdf)"
-# echo "***********************************************************"
-# for w in compiled/t-*.fst; do   
+#1 - generates files
+echo "\n***********************************************************"
+echo "Testing 'OCT/31/2025' and 'OUT/7/2025 with date2text.fst "
+echo "(the output is a transducer: fst and pdf)"
+echo "***********************************************************"
+for w in compiled/t-*.fst; do   
 
-#     # extract name from w:
-#     # file_path=$w
-#     # filename=$(basename "$file_path")
-#     # filename_without_extension="${filename%.fst}"
-#     # filename_without_prefix="${filename_without_extension#t-}"
-#     # "compiled/$filename_without_prefix.fst"
+    # NOTICE: the file is hard coded here 
+    fstcompose $w compiled/date2text.fst | fstshortestpath | fstproject --project_type=output |
+                  fstrmepsilon | fsttopsort > compiled/$(basename $w ".fst")-out.fst
+done
 
-#     # THE FILE IS HARD CODED HERE 
-#     fstcompose $w compiled/mmm2mm.fst | fstshortestpath | fstproject --project_type=output |
-#                   fstrmepsilon | fsttopsort > compiled/$(basename $i ".fst")-out.fst
-# done
-# for i in compiled/t-*-out.fst; do
-# 	echo "Creating image: images/$(basename $i '.fst').pdf"
-#    fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $i | dot -Tpdf > images/$(basename $i '.fst').pdf
-# done
+for i in compiled/t-*-out.fst; do
+	echo "Creating image: images/$(basename $i '.fst').pdf"
+    fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $i | dot -Tpdf > images/$(basename $i '.fst').pdf
+done
 
 
-# #2 - present the output as an acceptor
-# echo "\n***********************************************************"
-# echo "Testing 1 2 3 4 (output is a acceptor)"
-# echo "***********************************************************"
-# trans=n2text.fst
-# echo "\nTesting $trans"
-# for w in "1" "2" "3" "4"; do
-#     echo "\t $w"
-#     python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
-#                      fstcompose - compiled/$trans | fstshortestpath | fstproject --project_type=output |
-#                      fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
-# done
 
-# #3 - presents the output with the tokens concatenated (uses a different syms on the output)
-# fst2word() {
-# 	awk '{if(NF>=3){printf("%s",$3)}}END{printf("\n")}'
-# }
-
-# trans=n2text.fst
-# echo "\n***********************************************************"
-# echo "Testing 5 6 7 8  (output is a string  using 'syms-out.txt')"
-# echo "***********************************************************"
-# for w in 5 6 7 8; do
-#     res=$(python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
-#                        fstcompose - compiled/$trans | fstshortestpath | fstproject --project_type=output |
-#                        fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./scripts/syms-out.txt | fst2word)
-#     echo "$w = $res"
-# done
-
-# echo "\nThe end"
-
-
-# TESTING WITH V3
+#3 - presents the output with the tokens concatenated (uses a different syms on the output)
+# Test the transducers mix2numerical.fst, en2pt.fst, datenum2text.fst, mix2text.fst, and date2text.fst with
+# the dates on which the members of the working group turned 18.
 
 fst2word() {
 	awk '{if(NF>=3){printf("%s",$3)}}END{printf("\n")}'
 }
 
-trans=date2text.fst
-test_string="OCT/31/2025"
-# test_string="10/31/2025"
+# test numeric and mixed format
+bd1_mix="JUL/07/2014"
+bd1_num="7/07/2014"
+bd2_mix="OCT/1/2012"
+bd2_num="10/1/2012"
 
 echo "\n***********************************************************"
-echo "Testing $test_string (output is a string  using 'syms-out.txt')"
+echo "Testing $bd1_mix, $bd1_num, $bd2_mix and $bd2_num \n(output is a string  using 'syms-out.txt')"
 echo "***********************************************************"
-for w in $test_string; do
 
-    res=$(python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
-                       fstcompose - compiled/$trans | fstshortestpath | fstproject --project_type=output |
-                       fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./scripts/syms-out.txt | fst2word)
-    echo "$w = $res"
+for t in "mix2numerical.fst" "en2pt.fst" "datenum2text.fst" "mix2text.fst" "date2text.fst"; do
+    echo "\nTesting $t"
+    for w in $bd1_mix $bd1_num $bd2_mix $bd2_num; do
+
+        res=$(python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
+                        fstcompose - compiled/$t | fstshortestpath | fstproject --project_type=output |
+                        fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./scripts/syms-out.txt | fst2word)
+        echo "$w = $res"
+    done
 done
 
 echo "\nThe end"
