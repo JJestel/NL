ff3db08431030892cad29f3e239333b0a7e3dd57
Jascha Jestel <jj.jestel@gmail.com>
2023-09-29 18:30:51 +0100

    imroved testung

--- MP1/run.sh
+++ MP1/run.sh
@@ -12,21 +12,34 @@ done
 
 # ############ CORE OF THE PROJECT  ############
 
-fstconcat compiled/mmm2mm.fst compiled/mix2numerical_dy.fst > compiled/mix2numerical.fst
+# concat mmm2mm and identity to create mix2numerical
+# this would accept faulty data but as statesd in the project only valid dates are used
+fstconcat compiled/mmm2mm.fst compiled/identity.fst > compiled/mix2numerical.fst
 
+
+# invert pt2en to create en2pt
 fstinvert compiled/pt2en.fst > compiled/en2pt.fst
 
-fstconcat compiled/month.fst compiled/slash.fst > compiled/temp0.fst
-fstconcat compiled/temp0.fst compiled/day.fst > compiled/temp1.fst
-fstconcat compiled/temp1.fst compiled/comma.fst > compiled/temp2.fst
-fstconcat compiled/temp2.fst compiled/year.fst > compiled/datenum2text.fst
-rm compiled/temp*.fst
-rm compiled/slash.fst
-rm compiled/comma.fst
 
+# concat month, day and year to create datenum2text
+# use helper transducers comma and slash in between
+fstconcat compiled/month.fst compiled/h_slash.fst > compiled/temp0.fst
+fstconcat compiled/temp0.fst compiled/day.fst > compiled/temp1.fst
+fstconcat compiled/temp1.fst compiled/h_comma.fst > compiled/temp2.fst
+fstconcat compiled/temp2.fst compiled/year.fst > compiled/datenum2text.fst
+# remove temporary files
+rm compiled/temp*.fst 
+# rm compiled/slash.fst
+# rm compiled/comma.fst
+
+
+# compose mix2numerical and datenum2text to create mix2text
 fstcompose compiled/mix2numerical.fst compiled/datenum2text.fst > compiled/mix2text.fst
 
+
+# union of mix2text and datenum2text gives date2text
 fstunion  compiled/mix2text.fst compiled/datenum2text.fst > compiled/date2text.fst
+
 
 # ############ generate PDFs  ############
 echo "Starting to generate PDFs"
@@ -67,19 +80,41 @@ fst2word() {
 	awk '{if(NF>=3){printf("%s",$3)}}END{printf("\n")}'
 }
 
-# test numeric and mixed format
-bd1_mix="JUL/07/2014"
-bd1_num="7/07/2014"
-bd2_mix="OCT/1/2012"
-bd2_num="10/1/2012"
+# test numeric and mixed format of birthdays
+bd1_mix="JUL/25/2014"
+bd1_num="07/25/2014"
+bd2_mix="MAI/24/2018"
+bd2_num="5/24/2018"
 
 echo "\n***********************************************************"
-echo "Testing $bd1_mix, $bd1_num, $bd2_mix and $bd2_num \n(output is a string  using 'syms-out.txt')"
+echo "Testing 18th birthdays of group members (output is a string  using 'syms-out.txt')"
 echo "***********************************************************"
 
 for t in "mix2numerical.fst" "en2pt.fst" "datenum2text.fst" "mix2text.fst" "date2text.fst"; do
-    echo "\nTesting $t"
+    echo "\nTesting: $t"
     for w in $bd1_mix $bd1_num $bd2_mix $bd2_num; do
+        
+        res=$(python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
+                        fstcompose - compiled/$t | fstshortestpath | fstproject --project_type=output |
+                        fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./scripts/syms-out.txt | fst2word)
+        echo "$w = $res"
+    done
+done
+
+echo "\nThe end"
+
+
+
+
+# Additional tests
+
+echo "\n***********************************************************"
+echo "Testing some more corner cases"
+echo "***********************************************************"
+
+for t in "date2text.fst"; do
+    echo "\nTesting $t"
+    for w in "FEV/1/2099" "2/01/2099"; do
 
         res=$(python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
                         fstcompose - compiled/$t | fstshortestpath | fstproject --project_type=output |
@@ -89,3 +124,4 @@ for t in "mix2numerical.fst" "en2pt.fst" "datenum2text.fst" "mix2text.fst" "date
 done
 
 echo "\nThe end"
+
