885288de33c5a3e10da66aa475a993895808edf6
Jascha Jestel <jj.jestel@gmail.com>
2023-10-01 17:29:23 +0100

    added report

--- MP1/scripts/word2fst.py
+++ MP1/scripts/word2fst.py
@@ -9,13 +9,15 @@ if __name__ == "__main__":
     args = PARSER.parse_args()
     input = args.word
 
-    # read syms.txt file 
-    with open('./syms.txt', 'r') as f:
+    # PARSE INPUT USING THE SYMS FROM THE SYMS.TXT FILE
+    
+    # read syms.txt file
+    with open("./syms.txt", "r") as f:
         syms = f.readlines()
-    
+
     # get first word of each line using re
-    syms = [re.findall(r'^\S+', x)[0] for x in syms]
-    
+    syms = [re.findall(r"^\S+", x)[0] for x in syms]
+
     # sort syms by length to ensure longest match
     syms.sort(key=len, reverse=True)
 
@@ -27,14 +29,14 @@ if __name__ == "__main__":
             if str(input).startswith(s):
                 # remove s from input and add to tokens
                 tokens += [s]
-                input = input[len(s):]
+                input = input[len(s) :]
                 break
-        
+
         # throw error if no match found
         if input != "" and s == syms[-1]:
             print("ERROR: no match found for %s" % input)
             exit(1)
-    
+
     # create FST
     for i, c in enumerate(tokens):
         print("%d %d %s %s" % (i, i + 1, c, c))
